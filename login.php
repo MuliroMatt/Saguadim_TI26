194882ec92579c6ad58f5ca048e0ba0c148867d7
MuliroMatt <murilo.mattiuzzi@gmail.com>
2023-11-08 17:18:39 -0300

    criação da pagina de login e cadastro de usuário 08/11

--- login.php
+++ login.php
@@ -1,29 +1,40 @@
 |<?php
+//*INICIA VARIAVEL DE SESSÃO
 session_start();
 
+//*INCLUE CÓDIGO DE CONEXÃO DO BANCO
 include("conectadb.php");
 
+//*APÓS CLICK NO FORM POST
 if($_SERVER['REQUEST_METHOD']=='POST'){
-    $login = $_POST['login'];
+    $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT COUNT(usu_id) FROM usuarios WHERE usu_login = '$login' AND usu_senha = '$senha' AND usu_status = 's'";
-    #GRAVA LOG  
-    $sqllog = "INSERT INTO tab_log (tab_query, tab_data) VALUES ('$sql', NOW())";
+    //*QUERY DE VALIDA SE USUÁRIO EXISTE
+    $sql = "SELECT COUNT(usu_id) FROM usuarios WHERE usu_email = '$email' AND usu_senha = '$senha' AND usu_status = 's'";
+    $retorno = mysqli_query($link, $sql);
+
+    //*SUGESTÃO ARIEL DE SANITIZAÇÃO
+    $retorno = mysqli_fetch_array($retorno)[0];
+    
+    //*GRAVA LOG  
+    $sql = '"'.$sql.'"';
+    $sqllog = "INSERT INTO tab_log (tab_query, tab_data) VALUES ($sql, NOW())";
     mysqli_query($link, $sqllog);
 
-    $retorno = mysqli_query($link, $sql);
-    while($tbl = mysqli_fetch_array($retorno)){
-        $resultado = $tbl[0];
-    }
-
-    if($resultado == 0){
+    //*SE USUÁRIO NÃO EXISTE LOGA, SE NÃO, NÃO LOGA
+    if($retorno == 0){
         echo"<script>window.alert('USUÁRIO INCORRETO');</script>";
+        echo"<script>window.location.href='login.html';</script>";
+
     }
     else{
-        $sql = "SELECT * FROM usuarios WHERE usu_login = '$login' AND usu_senha = '$senha' AND usu_status = 's'";
+        $sql = "SELECT * FROM usuarios WHERE usu_email = '$email' AND usu_senha = '$senha' AND usu_status = 's'";
         $retorno = mysqli_query($link, $sql);
-        $sqllog = "INSERT INTO tab_log (tab_query, tab_data) VALUES ('$sql', NOW())";
+
+        //*GRAVA LOG
+        $sql = '"'.$sql.'"';
+        $sqllog = "INSERT INTO tab_log (tab_query, tab_data) VALUES ($sql, NOW())";
         mysqli_query($link, $sqllog);
         while($tbl = mysqli_fetch_array($retorno)){
             $_SESSION['idusuario'] = $tbl[0];
