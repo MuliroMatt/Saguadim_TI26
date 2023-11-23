a78a74b31fc951dbb1cf9582a074129d963dc7fd
MuliroMatt <murilo.mattiuzzi@gmail.com>
2023-11-23 16:57:47 -0300

    vambora

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById("toggleCadastra").addEventListener("click", function () {
     document.getElementById("login").style.display = "none";
-    document.getElementById("cadastra").style.display = "block";
+    document.getElementById("cadastra").style.display = "flex";
 });
 
 
 
 document.getElementById("toggleLogin").addEventListener("click", function () {
     document.getElementById("cadastra").style.display = "none";
-    document.getElementById("login").style.display = "block";
+    document.getElementById("login").style.display = "flex";
 });
 
